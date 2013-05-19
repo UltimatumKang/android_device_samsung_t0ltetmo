badb03dab078ddaad2ac3a8e541716c38519a03a
David Hou <david.hou314@gmail.com>
2013-05-18 20:52:13 -0700

    This seems to work

--- ukg.mk
+++ ukg.mk
@@ -2,19 +2,19 @@
 $(call inherit-product, device/samsung/t0ltetmo/full_t0ltetmo.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/ukg/config/common.mk)
 
 # t0lte Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/t0lte
+PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/t0lte
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/aosp/config/gsm.mk)
+$(call inherit-product, vendor/ukg/config/gsm.mk)
 
 # Tablet Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/common
 
 # Setup device specific product configuration.
-PRODUCT_NAME := xylon_t0ltetmo
+PRODUCT_NAME := ukg_t0ltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := t0ltetmo
 PRODUCT_MODEL := SGH-T889
@@ -25,7 +25,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltetmo TARGET_DEVICE=t0ltetmo BUI
 
 # Boot Animation
 PRODUCT_COPY_FILES +=  \
-    vendor/aosp/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
-    vendor/aosp/prebuilt/bootanimation/bootanimation_1280_720.zip:system/media/bootanimation.zip \
-    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/ukg/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
+    vendor/ukg/prebuilt/bootanimation/bootanimation_1280_720.zip:system/media/bootanimation.zip \
+    vendor/ukg/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/ukg/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
